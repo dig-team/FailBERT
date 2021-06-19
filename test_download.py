2bccd017f6c0bc18ab4bc50079e64fae4ffd22b7
Chadi <chadi.helwe@gmail.com>
2021-06-19 22:26:44 +0300

    deleted test_download.py

--- test_download.py
+++
@@ -1,6 +0,0 @@
-from failBERT.utils import download_pretrained_models
-
-download_pretrained_models(
-    "https://www.dropbox.com/s/bxjmcrs7p737zfx/padded_best_model_swapped_natural_dyck.pkl?dl=1",
-    "best_model_natural_dyck_2.pkl",
-)
